4e1687a1321aab729a054f00dfa8525aad293b37
praveen <praveenpp2929@gmail.com>
2025-07-29 21:07:47 +0200

    updates

--- StreamLogic.lean
+++ StreamLogic.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `StreamLogic` library.
 -- Import modules here that should be built as part of the library.
 import StreamLogic.Basic
+import StreamLogic.TutorialOne
